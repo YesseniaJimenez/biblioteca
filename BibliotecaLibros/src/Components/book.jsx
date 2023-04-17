cc1515d235abe73b5a8589a86c7872dbeda29e77
yessenia <yejiga2006@gmail.com>
2023-04-17 17:59:55 -0500

    mejorando estilos

--- BibliotecaLibros/src/Components/book.jsx
+++ BibliotecaLibros/src/Components/book.jsx
@@ -1,9 +1,21 @@
 import { Link } from "react-router-dom";
 
 export default function Book({ item }) {
+  const booksContainerStyle = {
+    display: "flex",
+    flexDirection: "colunm",
+    width: "300px",
+  };
+
+  const bookInfoStyle = {
+    display: "flex",
+    flexDirection: "colunm",
+    alingItems: "center",
+    textAling: "center",
+  };
   return (
-    <div>
-      <Link to={`/view/${item.id}`}>
+    <div style={booksContainerStyle}>
+      <Link to={`/view/${item.id}`} style={bookInfoStyle}>
         <img src={item.cover} width="200" alt={item.title} />
         <div>{item.title}</div>
       </Link>
